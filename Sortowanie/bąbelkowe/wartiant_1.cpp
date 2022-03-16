549fc7230f66a24b4341b80a02e1fde524044f7b
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2022-03-16 13:52:22 +0100

    Wariant 1 - sortowanie bąbelkowe

---
+++ Sortowanie/bąbelkowe/wartiant_1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <time.h>
+#include <math.h>
+using namespace std;
+
+const int
+    LEN = 10,       // długość tablicy
+    MAX_VALUE = 10; // maksymalna wartość wyrazu
+
+void zapisz(int tab[LEN])
+{
+    srand(time(NULL));
+    for (int i = 0; i < LEN; i++)
+        tab[i] = rand() % MAX_VALUE + 1;
+}
+
+void wypisz(int tab[LEN])
+{
+    srand(time(NULL));
+    for (int i = 0; i < LEN; i++)
+        cout << tab[i] << ", ";
+    cout << "\n";
+}
+
+int main()
+{
+    int tablica[LEN]; // tablica z liczbami
+    zapisz(tablica);
+    cout << "Przed sortowaniem:\n";
+    wypisz(tablica);
+
+    int p,     // pozycja gdzie została dokonana ostatnia zamiania
+        e = 0; // licznik obrotów pętli
+        
+    cout << "\n";
+    do
+    {
+        p = -1;                              // żeby nie wpaśc w nieskończoną pętlę
+        for (int k = 0; k < LEN; k++)        // k - kursor
+            if (tablica[k] > tablica[k + 1]) // jeżeli lewy wyraz ma większą wartość od lewego
+            {
+                swap(tablica[k], tablica[k + 1]); // zamień te wartości miejscami
+                p = k;
+            }
+        e++;
+        cout << " Etap " << e << ":  ";
+        wypisz(tablica);
+    } while (p >= 0);
+    cout << "\n\nEfekt koncowy:\n";
+    wypisz(tablica);
+}
