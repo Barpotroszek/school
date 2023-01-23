c4b6ee171c21cadacd1f69b97a4978336931a232
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2023-01-23 11:54:01 +0100

    Delete SQL directory

--- SQL/zadanie5_3.sql
+++
@@ -1,9 +0,0 @@
-SELECT nr_pokoju FROM `pokoje_txt`
-WHERE standard = "N" AND 
-nr_pokoju NOT IN
-	(SELECT nr_pokoju FROM noclegi_txt
-	INNER JOIN klienci_txt
-	ON klienci_txt.nr_dowodu=noclegi_txt.nr_dowodu
-	WHERE miejscowosc IN ("Opole", "Katowice")
-   );
-    
